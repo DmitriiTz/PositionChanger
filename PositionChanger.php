a0873559dac7698baf8157404ca38e4bbd9599fe
Cibulsky Dmitry <d.cibulsky@oyster-telecom.ru>
2020-04-10 18:15:21 +0300

    Rename  variable to

--- PositionChanger.php
+++ PositionChanger.php
@@ -13,27 +13,27 @@ class PositionChanger extends Controller
         //Если при обновлении изменяется позиция
         if (array_key_exists('position', $request->all())) {
             //Находим колонку, которую изменяем
-            $this_column = Column::find($id);
+            $column = Column::find($id);
             //Находим лимит колонок которым нужно изменить значение
-            $limit = abs($this_column->position - $request->position);
+            $limit = abs($column->position - $request->position);
             $increment = 1;
             $comparison = '<=';
             $direction = 'desc';
 
             //Если новая позиция меньше старой
-            if ($this_column->position > $request->position) {
+            if ($column->position > $request->position) {
                 $increment = -1;
                 $comparison = '>=';
                 $direction = 'asc';
             }
 
             //Тогда изменяем те что выше/ниже
-            Column::where('board_id', $this_column->board_id)
+            Column::where('board_id', $column->board_id)
                 ->where('position', $comparison, $request->position)
                 ->orderBy('position', $direction)
                 ->limit($limit)
                 ->get()
-                ->except($this_column->id)
+                ->except($column->id)
                 //Изменяем у каждого значение
                 ->each(function ($item) use ($increment) {
                     $item['position'] -= $increment;
